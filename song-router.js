ac2ab531fb792d964998f337dc26674caed5928f
mihail.gaberov <mihail.gaberov@ysura.com>
2022-08-16 11:11:52 +0200

    👨🏻‍🎓 Adding REST routes for CRUD

--- song-router.js
+++ song-router.js
@@ -23,3 +23,37 @@ router.put("/", async (req, res) => {
   // return the id of the newly created Song
   res.send({ id });
 });
+
+router.get("/:id", async (req, res) => {
+  // fetch the Song
+  let song = await repository.fetch(req.params.id);
+
+  // return the Song we just fetched
+  res.send(song);
+});
+
+router.get("/", async (req, res) => {
+  let songs = await repository.search().returnAll();
+  res.send(songs);
+});
+
+router.post("/:id", async (req, res) => {
+  // fetch the Song we are replacing
+  let song = await repository.fetch(req.params.id);
+
+  // set all the properties, converting missing properties to null
+  song.title = req.body.title ?? null;
+  song.artist = req.body.artist ?? null;
+  song.genres = req.body.genres ?? null;
+  song.lyrics = req.body.lyrics ?? null;
+  song.music = req.body.music ?? null;
+  song.year = req.body.year ?? null;
+  song.duration = req.body.duration ?? null;
+  song.link = req.body.link ?? null;
+
+  // save the Song to Redis
+  let id = await repository.save(song);
+
+  // return the id of the Song we just saved
+  res.send({ id });
+});
